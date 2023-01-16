6b459a2380e9ec577d9d8a53698e5296c9a41342
Kamalyunus <yunus.kamal@gmail.com>
2023-01-16 13:28:40 -0600

    sample data example

--- src/pricing_optimize.py
+++ src/pricing_optimize.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import differential_evolution, NonlinearConstraint, Bounds
+import sample_data as sp
 
 def objective_func(x, e, bp, bq):
     perc_qty_change = np.multiply(e,x)
@@ -16,12 +17,7 @@ def investment(x,bp,bq):
 
 if __name__ == '__main__':
 
-    #Sample Data
-    e = np.array([-1.70, -1.75, -1.87, -2.00, -2.14, -2.30]) # Price Elasticities
-    bp = np.array([4.1, 2.3, 3.2, 4.5, 10.1, 5.4]) # Item Current Prices
-    bq = np.array([100,	120, 340, 50, 25, 150]) # Item Baseline Forecast
-
-    num_items = e.size # number of items
+    num_items = sp.e.size # number of items
     
     #Constraints
     max_price_change = 0.2 # Constraint: Maximum % Price Change allowed per Item
@@ -30,20 +26,20 @@ if __name__ == '__main__':
     #Solver
     best = differential_evolution(
             objective_func,
-            x0 = -max_price_change*np.ones(e.size)*0.5,
-            args=(e,bp,bq),
-            bounds=Bounds(lb=-max_price_change*np.ones(e.size), ub=np.zeros(e.size)),
-            constraints=NonlinearConstraint(lambda x: investment(x,bp,bq), lb = 0, ub = budget),
+            x0 = -max_price_change*np.ones(num_items)*0.5,
+            args=(sp.e,sp.bp,sp.bq),
+            bounds=Bounds(lb=-max_price_change*np.ones(num_items), ub=np.zeros(num_items)),
+            constraints=NonlinearConstraint(lambda x: investment(x,sp.bp,sp.bq), lb = 0, ub = budget),
             seed = 1234
     )
 
     # Print Results
     if best.success:
-        new_price = bp + np.multiply(bp,best.x)
-        perc_qty_change = np.multiply(e,best.x)
-        new_qty = bq + np.multiply(perc_qty_change,bq)
-        baseline_revenue = np.dot(bp,bq)
-        baseline_qty=sum(bq)
+        new_price = sp.bp + np.multiply(sp.bp,best.x)
+        perc_qty_change = np.multiply(sp.e,best.x)
+        new_qty = sp.bq + np.multiply(perc_qty_change,sp.bq)
+        baseline_revenue = np.dot(sp.bp,sp.bq)
+        baseline_qty=sum(sp.bq)
 
         print(f"Optimal solution found")
         print(f"Baseline Revenue: ${round(baseline_revenue, 0)}, Optimize Revenue: ${-round(best.fun, 0)}, Revenue Increase: {round(((-best.fun/baseline_revenue)-1)*100,1)}%")
@@ -51,7 +47,6 @@ if __name__ == '__main__':
         
         for item in range(0,len(best.x)):
             print(f"Optimal Item {item+1} Price Change: {round(best.x[item]*100,1)}% with New Selling Price: ${round(new_price[item],2)}")
-
         
     else: 
         print("No solution found to problem")
---
+++ src/sample_data.py
@@ -0,0 +1,6 @@
+import numpy as np    
+
+#Sample Data
+e = np.array([-1.70, -1.75, -1.87, -2.00, -2.14, -2.30]) # Price Elasticities
+bp = np.array([4.1, 2.3, 3.2, 4.5, 10.1, 5.4]) # Item Current Prices
+bq = np.array([100,	120, 340, 50, 25, 150]) # Item Baseline Forecast
